a03c910c43273cb71267fa2190e1a7384b5b95fa
allanfantoni <allan.fantoni@gmail.com>
2020-03-28 18:46:57 -0300

    code refactored and modal implemented

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,3 +11,23 @@ function setBoldLink() {
   else if (urlCurrentPage === recipesLink.href)
     recipesLink.classList.add('bold-link');
 }
+
+const modalOverlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+  card.addEventListener('click', function() {
+    const image = card.getElementsByTagName('img')[0].alt;
+    const title = card.getElementsByTagName('h3')[0].textContent;
+    const info = card.getElementsByTagName('p')[0].textContent;
+    modalOverlay.classList.add('active');
+    modalOverlay.querySelector('img').src = `layouts/assets/${image}.png`;
+    modalOverlay.querySelector('h3').textContent = title;
+    modalOverlay.querySelector('p').textContent = info;
+  });
+}
+
+const closeModal = document.querySelector('.close-modal').addEventListener('click', function() {
+  modalOverlay.classList.remove('active');
+});
